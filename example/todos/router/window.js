009d6adf59c08c0e5bc67dcdfc593d159d134f0a
Izaak Schroeder <izaak.schroeder@gmail.com>
2015-05-17 21:15:32 -0700

    WIP.

--- example/todos/router/window.js
+++ example/todos/router/window.js
@@ -1,6 +1,12 @@
 if (window && window.history && window.history.pushState) {
-	// window.addEventListener('popstate', listener);
+	//
 	// window.removeEventListener('popstate', listener)
+	function listener(state) {
+		router.navigate(state.url);
+	}
+
+	window.addEventListener('popstate', listener);
+
 	router.url.observe(function() {
 		// Also replaceState
 		window.history.pushState(state, title, url);
